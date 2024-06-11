31390e1bb86c1ccb049f9910583f5e3166db3ead
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-06-10 20:31:58 -0700

    Enabling static page generation -> no longer server side rendered

---
+++ my-blog/components/PostPreview.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+import { PostMetadata } from "./PostMetaData";
+
+function PostPreview(props: PostMetadata) {
+  return (
+    <div>
+      <Link href={`/posts/${props.slug}`}>
+        <h2>{props.title}</h2>
+      </Link>
+      <p>{props.subtitle}</p>
+      <p>{props.date}</p>
+    </div>
+  );
+}
+
+export default PostPreview;
