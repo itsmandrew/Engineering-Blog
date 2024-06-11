249efa3c55bc61f111cf2aa05c70abaaf98e2ab3
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-06-10 20:17:27 -0700

    Graymatter parsing

--- my-blog/app/posts/[slug]/page.tsx
+++ my-blog/app/posts/[slug]/page.tsx
@@ -1,23 +1,24 @@
 import fs from "fs";
 import Markdown from "markdown-to-jsx";
+import matter from "gray-matter";
 
 const getPostContent = (slug: string) => {
-    const folder = "posts/";
-    const file = `${folder}${slug}.md`;
-    const content = fs.readFileSync(file, "utf-8");
-    return content
-}
-
+  const folder = "posts/";
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync(file, "utf-8");
+  const matterResult = matter(content);
+  return matterResult;
+};
 
 const PostPage = (props: any) => {
-    const slug = props.params.slug;
-    const content = getPostContent(slug);
-    return (
-        <div>
-            <h1>This a post: {slug} </h1>
-            <Markdown>{content}</Markdown>
-        </div>
-    )
-}
+  const slug = props.params.slug;
+  const post = getPostContent(slug);
+  return (
+    <div>
+      <h1>{post.data.title}</h1>
+      <Markdown>{post.content}</Markdown>
+    </div>
+  );
+};
 
 export default PostPage;
