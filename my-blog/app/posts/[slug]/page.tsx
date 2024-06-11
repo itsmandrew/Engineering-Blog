55946c4bf6af6aa5b43d0cad0b7ae04f4398e0ea
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-06-10 19:56:59 -0700

    Added so content is displayed in MARKDOWN

--- my-blog/app/posts/[slug]/page.tsx
+++ my-blog/app/posts/[slug]/page.tsx
@@ -1,9 +1,22 @@
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
+
+const getPostContent = (slug: string) => {
+    const folder = "posts/";
+    const file = `${folder}${slug}.md`;
+    const content = fs.readFileSync(file, "utf-8");
+    return content
+}
+
+
 const PostPage = (props: any) => {
     const slug = props.params.slug;
+    const content = getPostContent(slug);
     return (
-        <p>
+        <div>
             <h1>This a post: {slug} </h1>
-        </p>
+            <Markdown>{content}</Markdown>
+        </div>
     )
 }
 
